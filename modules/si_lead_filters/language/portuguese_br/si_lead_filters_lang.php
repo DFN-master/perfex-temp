ca5975542facb98496d676edbbc612f908c5955a
feplyss <fellipe.bomfim10@gmail.com>
2025-01-21 14:29:16 -0300

    feat: translating lead filter module

---
+++ modules/si_lead_filters/language/portuguese_br/si_lead_filters_lang.php
@@ -0,0 +1,32 @@
+<?php
+
+# Version 1.0.0
+$lang['si_lead_filters']		= 'Filtros de Lead';
+$lang['si_lead_filters_menu']	= 'Filtros de Lead';
+$lang['si_lf_submenu_lead_filters']         = 'Filtros de Lead';
+$lang['si_lf_submenu_filter_templates']     = 'Modelos de Filtros de Lead';
+
+$lang['si_lf_lead_filter_by_date'] = 'Filtrar por Data';
+$lang['si_lf_save_filter_template'] = 'Template Selecionar o demarcado para Salvar Filtro';
+$lang['si_lf_filter_template_name'] = 'Salvar como Modelo de Filtro (Adicione o nome do Filtro aqui)';
+$lang['si_lf_no_filter_template'] = 'Nenhum Modelo de Filtro salvo';
+$lang['si_lf_filter_templates'] = 'Lista de Modelos de Filtro';
+$lang['si_lf_filter_name'] = 'Nome do Filtro';
+$lang['si_lf_delete_confirm']='Tem certeza que quer excluir esse Modelo de Filtro?';
+$lang['si_lf_add_lead_filter']='Adicionar Filtro de Lead';
+$lang['si_lf_hide_export_columns']='Esconder colunas';
+$lang['si_lf_hide_export_columns_info']='Esconder colunas da exportação (Escolha as colunas que quer esconder, e clique no botão do Filtro)';
+$lang['si_lf_apply_filter']='Aplicar Filtro e salvar Modelo';
+$lang['si_lf_filter_by_type'] = 'Tipo de Lead';
+$lang['si_lf_unknown'] = 'Desconhecido';
+$lang['si_lf_new'] = 'Novo';
+//for datatable
+$lang['si_lf_created_date'] = 'Data de criação';
+$lang['si_lf_last_contacted_date'] = 'Último contato';
+#V1.1.5
+$lang['si_lf_title_settings']     = 'Configurações de Filtros de Lead';
+$lang['si_lf_settings_cf_text']   = 'Selecione campos customizados para Filtrar (Max. '.SI_LEAD_FILTERS_MAX_CUSTOM_FIELDS.')';
+#Version 1.1.9
+$lang['si_lf_new_info'] = 'Novo Filtro (Limpo)';
+$lang['si_lf_set_as_default'] = 'Definir como padrão';
+$lang['si_lf_set_as_default_info'] = 'Definir como Modelo padrão ao carregar Filtro de Lead';
